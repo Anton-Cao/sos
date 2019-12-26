0fab2ae012430caaa84b39625fbedcd816c1b09a
Anton-Cao <cao.anton@gmail.com>
2019-12-25 19:08:27 -0800

    separate logic for parent and child

--- src/sos.c
+++ src/sos.c
@@ -14,6 +14,87 @@
 
 #define MAX_COMMAND_TOKENS 8
 
+const char *prompt = "(sos)";
+
+void child(struct cmd_opts *cmd_opts, int minionpt)
+{
+  char *token = strtok(cmd_opts->command, " ");
+  char *argv[MAX_COMMAND_TOKENS];
+  int i = 0;
+  while (token != NULL) {
+    if (i >= MAX_COMMAND_TOKENS) {
+      fprintf(stderr, "maximum token limit exceeded\n");
+      exit(-1);
+    }
+    argv[i++] = token;
+    token = strtok(NULL, " ");
+  }
+  argv[i] = NULL;
+#ifdef DEBUG
+  printf("command: %s\n", cmd_opts->command);
+  for (int j = 0; j < i; j++)
+    printf("arg: %s\n", argv[j]);
+#endif
+  close(STDIN_FILENO);
+  dup(minionpt);
+  close(STDOUT_FILENO);
+  dup(minionpt);
+  close(STDERR_FILENO);
+  dup(minionpt);
+#ifdef DEBUG
+  printf("entering the warp zone...\n");  // this will be written to command_stdout
+#endif
+  execvp(argv[0], (char * const *) argv);
+}
+
+void parent(struct cmd_opts *cmd_opts, int masterpt)
+{
+  int i = 0, status, len;
+  char c,
+    buf[MAX_LINE_LEN],
+    err[MAX_LINE_LEN],
+    *query,
+    url[MAX_LINE_LEN];
+  struct error_filter *filter;
+  regmatch_t rms[2];
+  CURL *curl = curl_easy_init();
+
+  while (read(masterpt, &c, sizeof(c))) {
+    if (c == '\n') {
+      buf[i] = 0;
+      i = 0;
+      printf("%s\n", buf);
+
+      filter = error_filters;
+      while (filter) {
+        status = regexec(&filter->re, buf, 2, rms, 0);
+        if (status == 0) {  // match found
+          len = rms[1].rm_eo - rms[1].rm_so;
+          strncpy(err, buf + rms[1].rm_so, len);
+          err[len] = 0;
+          query = curl_easy_escape(curl, err, len);
+          snprintf(url, MAX_LINE_LEN, "https://stackoverflow.com/search?q=%s", query);
+          curl_free(query);
+          printf("%s \033[91m%s\033[0m\n", prompt, url);
+          if (cmd_opts->open_browser) {
+            if (fork() == 0) {  // spawn new process to open in browser
+              char *argv[] = {"open", url, NULL};
+              execvp(argv[0], argv);
+            }
+          }
+          break;
+        }
+        filter = filter->next;
+      }
+    } else {
+      buf[i++] = c;
+    }
+  }
+#ifdef DEBUG
+  printf("child exited\n");
+#endif
+}
+
 int main(int argc, char *argv[])
 {
   struct cmd_opts cmd_opts;
@@ -24,8 +105,6 @@ int main(int argc, char *argv[])
   if (ErrorFilter_parse(cmd_opts.sosrc_path) == -1)
     return -1;
 
-  // O_NOCTTY because we don't want parent process to be controlled by the pseudo-terminal
-  // https://linux.die.net/man/3/posix_openpt
   // https://stackoverflow.com/questions/4057985/disabling-stdout-buffering-of-a-forked-process
   // https://pubs.opengroup.org/onlinepubs/009695399/functions/posix_openpt.html
   int masterpt, minionpt;
@@ -47,81 +126,12 @@ int main(int argc, char *argv[])
   if (pid == 0) {
     // child process
     close(masterpt);
-    char *token = strtok(cmd_opts.command, " ");
-    char *argv[MAX_COMMAND_TOKENS];
-    int i = 0;
-    while (token != NULL) {
-      if (i >= MAX_COMMAND_TOKENS) {
-        fprintf(stderr, "maximum token limit exceeded\n");
-        exit(-1);
-      }
-      argv[i++] = token;
-      token = strtok(NULL, " ");
-    }
-    argv[i] = NULL;
-    #ifdef DEBUG
-    printf("command: %s\n", command);
-    for (int j = 0; j < i; j++)
-      printf("arg: %s\n", argv[j]);
-    #endif
-    close(STDIN_FILENO);
-    dup(minionpt);
-    close(STDOUT_FILENO);
-    dup(minionpt);
-    close(STDERR_FILENO);
-    dup(minionpt);
-    #ifdef DEBUG
-    printf("entering the warp zone...\n");  // this will be written to command_stdout
-    #endif
-    execvp(cmd_opts.command, (char * const *) argv);
+    child(&cmd_opts, minionpt);
   } else {
     // parent process
     close(minionpt);
-    printf("(sos) running [%s] with pid=%d\n", cmd_opts.command, pid);
-    char c;
-    char buf[MAX_LINE_LEN];
-    char errmsg[MAX_LINE_LEN];
-    char url[MAX_LINE_LEN];
-    int i = 0;
-    CURL *curl = curl_easy_init();
-    while (read(masterpt, &c, sizeof(c))) {
-      if (c == '\n') {
-        buf[i] = 0;  // null-terminate
-        i = 0;  // reset
-        printf("%s\n", buf);
-        struct error_filter *filter = error_filters;
-        regmatch_t rms[2];
-        int status;
-        while (filter) {
-          status = regexec(&filter->re, buf, 2, rms, 0);
-          if (status == 0) {
-            // match found
-            size_t len = rms[1].rm_eo - rms[1].rm_so;
-            strncpy(errmsg, buf + rms[1].rm_so, len);
-            errmsg[len] = 0;
-            char *query = curl_easy_escape(curl, errmsg, len);
-            snprintf(url, MAX_LINE_LEN, "https://stackoverflow.com/search?q=%s", query);
-            printf("(sos) \033[91m%s\033[0m\n", url);
-            curl_free(query);
-            if (cmd_opts.open_browser) {
-              printf("(sos) opening in browser...\n");
-              if (fork() == 0) {
-                // spawn new process to open in browser
-                char *argv[] = {"open", url, NULL};
-                execvp(argv[0], argv);
-              }
-            }
-            break;
-          }
-          filter = filter->next;
-        }
-      } else {
-        buf[i++] = c;
-      }
-    }
-    #ifdef DEBUG
-    printf("child exited\n");
-    #endif
+    printf("%s running [%s] with pid=%d\n", prompt, cmd_opts.command, pid);
+    parent(&cmd_opts, masterpt);
   }
 
   return 0;
